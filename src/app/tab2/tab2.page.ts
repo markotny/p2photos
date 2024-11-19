fac12318ec88af97afb702db03d0a7523177a30c
Marcin Kotas <markotny97@gmail.com>
2024-11-19 23:16:24 +0100

    initial

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,51 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonCol, IonGrid, IonRow, IonImg, IonInput } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { PhotoService } from '../services/photo.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgModel } from '@angular/forms';
+import * as WebTorrent from 'webtorrent'
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonInput, FormsModule, CommonModule, IonImg, IonRow, IonGrid, IonCol, IonIcon, IonFabButton, IonFab, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit, AfterViewInit {
 
-  constructor() {}
+  client!: WebTorrent.Instance;
+  torrent?: any;
 
+  files?: File[];
+
+  constructor(public photoService: PhotoService) { }
+
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+  
+  ngAfterViewInit() {
+    this.client = new (<any>window)['WebTorrent']();
+    console.log(this.client);
+  }
+  
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+  async seed(event: Event)
+  {
+    
+    const target = event.target as HTMLInputElement;
+    const files = target.files as FileList;
+    // if (!this.files) return;
+    
+    this.client.seed(files, torrent => {
+      console.log('Client is seeding:', torrent)
+      const {created, announce, magnetURI, name } = torrent;
+      this.torrent = {created, announce, magnetURI, name };
+    })
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,14 +1,68 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, AfterViewInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonItem, IonFabButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import * as WebTorrent from 'webtorrent'
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonFabButton, IonItem, CommonModule, FormsModule, IonInput, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
-export class Tab3Page {
-  constructor() {}
+export class Tab3Page implements AfterViewInit {
+  client!: WebTorrent.Instance;
+
+  magnetUri?: string;
+  fileNames: string[] = [];
+  constructor() { }
+  
+  ngAfterViewInit() {
+    this.client = new (<any>window)['WebTorrent']();
+  }
+
+  download() {
+    if (!this.magnetUri) return;
+
+    this.client.add(this.magnetUri, async torrent => {
+      // Got torrent metadata!
+      console.log('Client is downloading:', torrent)
+    
+      await this.share(torrent);
+    })
+  }
+
+  async share(torrent: WebTorrent.Torrent) {
+     if (!navigator.canShare) {
+       alert('sharing not supported');
+     }
+    
+    const files: File[] = [];
+    
+    torrent.files.forEach(torrentFile => {
+      torrentFile.getBlob((err, blob) => blob && files.push(new File([blob], torrentFile.name, {type: blob.type})))
+    });
+
+    torrent.on('done', () => {
+      console.log('done...');
+      setTimeout(async () => {
+        console.log('files to share', files.length, files, torrent.files)
+        if (navigator.canShare({ files })) {
+          try {
+            await navigator.share({
+              files,
+              title: "Images",
+              text: "Beautiful images",
+            });
+          } catch (error) {
+            alert(`Error: ${(error as any).message}`);
+          }
+        } else {
+          alert(`Your system doesn't support sharing these files.`);
+        }
+      }, 100);
+    });
+   }
 }
